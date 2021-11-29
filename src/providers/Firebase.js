87c73d25a3ab8150388c7191915e7fc465362d9d
boidushya <boidushyabhattacharya@gmail.com>
2021-11-30 02:14:12 +0530

    fix: minor styling fixes

--- src/providers/Firebase.js
+++ src/providers/Firebase.js
@@ -12,6 +12,15 @@ const firebaseConfig = {
   measurementId: "G-36MB6Y7J35"
 };
 
+// const firebaseConfig = {
+// 	apiKey: "AIzaSyDQPWh18lMx2Z91WjV_O-ZP2Pwythl8x8I",
+// 	authDomain: "symphony-5a87f.firebaseapp.com",
+// 	projectId: "symphony-5a87f",
+// 	storageBucket: "symphony-5a87f.appspot.com",
+// 	messagingSenderId: "569969649358",
+// 	appId: "1:569969649358:web:c487ecfd8340eb109f529f"
+// };
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,38 +1,19 @@
-export const blankClassroom = [
-  {
-    name: 'Student 1',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-  {
-    name: 'Student 2',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-  {
-    name: 'Student 3',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-  {
-    name: 'Student 4',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-  {
-    name: 'Student 5',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-  {
-    name: 'Student 6',
-    participantId: false,
-    isHandRaised: false,
-    isCalledOn: false,
-  },
-];
+import Axios from "axios";
+
+let blankClassroom=[];
+
+Axios.get("https://obsidian-backend.herokuapp.com/student/allStudents")
+	.then((res) => {
+		res.data.response.map((student) => (
+			blankClassroom.push(
+				{
+					name: student.name,
+					participantId: false,
+					isHandRaised: false,
+					isCalledOn: false
+				}
+			)
+		))
+	})
+
+export { blankClassroom };
